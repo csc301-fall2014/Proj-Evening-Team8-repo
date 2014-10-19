d28cce612a83b3106618c9cbf07c0f44fe61aa2e
bensonchan <bensonyk.chan@mail.utoronto.ca>
2014-10-19 01:17:40 -0400

    Added in missing files

---
+++ messageboard/mainsite/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from mainsite import views
+
+urlpatterns = [
+    url(r'^registration', views.registration, name='registration'),
+    url(r'^register', views.register, name='register'),
+]
