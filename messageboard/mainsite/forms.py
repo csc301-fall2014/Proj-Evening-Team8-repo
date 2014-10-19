b9556a0b142f8e862321d4ad4454a7fc79f38b2b
bensonchan <bensonyk.chan@mail.utoronto.ca>
2014-10-19 09:31:05 -0400

    Added first/last name fields
    Former-commit-id: 52167cc5a8f534d6ff6bf74e04086d540059e87b

--- messageboard/mainsite/forms.py
+++ messageboard/mainsite/forms.py
@@ -6,4 +6,4 @@ from django.forms import ModelForm
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'first_name', 'last_name']
