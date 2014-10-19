9b1db6417c8ff8e7e588f1f49c3e6d3cc2d75f88
bensonchan <bensonyk.chan@mail.utoronto.ca>
2014-10-19 01:16:04 -0400

    Added registration functionality
    Former-commit-id: 4efcc6db268d94ee2276bb4baa185c07a0cdcdaf

--- messageboard/mainsite/views.py
+++ messageboard/mainsite/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from mainsite.forms import UserForm
 
-# Create your views here.
+
+def registration(request):
+    return render(request, 'mainsite/registration.html', {'form': UserForm()})
+
+
+def register(request):
+    new_user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
+    return render(request, 'mainsite/register.html', {'new_user': new_user})
