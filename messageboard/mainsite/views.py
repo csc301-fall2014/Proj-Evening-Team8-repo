747e6a08658dee03ebc21b57b84cbe82d0c86f77
aleccool213 <alec.brunelle@mail.utoronto.ca>
2014-10-20 21:12:31 -0400

    more changes
    Former-commit-id: db37db45380fc0525f90e86925a6f8056f52d6da

--- messageboard/mainsite/views.py
+++ messageboard/mainsite/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from mainsite.forms import UserForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def registration(request):
@@ -21,4 +23,17 @@ def registration(request):
             return HttpResponse('Invalid Form Data.' + str(form.errors))
     else:
         # Registration not completed, initialize form
-        return render(request, 'mainsite/registration.html', {'form': UserForm(initial={'email': '@mail.utoronto.ca'})})
+        return render(request, 'mainsite/registration/registration.html', {'form': UserForm(initial={'email': '@mail.utoronto.ca'})})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page
+            return render(request, 'mainsite/', {'data': data})
+
+    else:
+        return render(request, 'mainsite/login/login.html', {'form': AuthenticationForm()})
