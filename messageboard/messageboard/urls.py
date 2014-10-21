ab863d69caafec1daabe927acd03093ecf3fba1f
aleccool213 <alec.brunelle@mail.utoronto.ca>
2014-10-20 21:36:12 -0400

    simple index page
    Former-commit-id: cba47f50d117b827866a64cd16d0486b028c2424

--- messageboard/messageboard/urls.py
+++ messageboard/messageboard/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from mainsite import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'messageboard.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^mainsite/', include('mainsite.urls', namespace="mainsite")),
+    url(r'^mainsite/', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
