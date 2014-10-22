797329f80e77cb126cbb707f9cc6dc505c597e44
Spencer Wahl <spencer.s.wahl@gmail.com>
2014-10-22 01:32:11 -0400

    Improved views checking for login
    Made messageboard and topic views use the login_required tag instead of
    an if statement, they now auto redirect to the login page
    
    
    Former-commit-id: 0c2fc4db3f93bb5611651172a56e068a35a1f670

--- messageboard/messageboard/urls.py
+++ messageboard/messageboard/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from mainsite import views
 
 urlpatterns = patterns('',
+    #check if it is landing page
+    url(r'^mainsite/$', views.index, name='index'),
 	#check for login/registration urls
     url(r'^mainsite/', include('mainsite.urls', namespace="mainsite")),
-    #check if it is landing page
-    url(r'^mainsite/', views.index, name='index'),
     #check for admin
     url(r'^admin/', include(admin.site.urls)),
 )
