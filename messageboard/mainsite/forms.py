c17bf1da3689a14880cb743e8e694b5a5ed48c74
Spencer Wahl <spencer.s.wahl@gmail.com>
2014-10-22 06:13:41 -0400

    Posting messages "works"
    It posts the message but refreshes the page as well, so its not optimal
    but its functional.
    
    
    Former-commit-id: 16da4cd88c3de6d1c1fd19916eabe4bf94ff2a45

--- messageboard/mainsite/forms.py
+++ messageboard/mainsite/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from mainsite.models import Message
 from django.forms import ModelForm
 
 
@@ -7,3 +8,13 @@ class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password', 'email',  'first_name', 'last_name']
+
+class MessageForm(ModelForm):
+    class Meta:
+        model = Message
+        exclude = ('pub_date','topic')
+        widgets ={
+            'message_content': forms.Textarea(attrs={'cols': 40, 'rows': 3,
+                                                     'placeholder': "Write your message here...",
+                                                     'required': True})
+        }
