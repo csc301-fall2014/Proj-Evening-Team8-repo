c3536726b1932233baf17285e83fe36491f7f774
bensonchan <bensonyk.chan@mail.utoronto.ca>
2014-10-23 12:46:24 -0400

    Fixed security bugs and login, tweaked registration.

--- messageboard/mainsite/models.py
+++ messageboard/mainsite/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-import datetime
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    activation_key = models.CharField(max_length=40, blank=True)
-    key_expires = models.DateTimeField(default=datetime.date.today())
+    activation_key = models.CharField(max_length=40, blank=True, unique=True)
+    key_expires = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.user.username
--- messageboard/mainsite/views.py
+++ messageboard/mainsite/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
+import hashlib
+import random
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from mainsite.forms import UserForm, MessageForm, TopicForm
@@ -6,10 +8,9 @@ from django.contrib.auth import authenticate, logout
 from django.contrib.auth import login as auth_login  # Changed name because login is our view function
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.hashers import make_password
 from django.core.mail import send_mail
-from django.core.exceptions import MultipleObjectsReturned
 from django.utils import timezone
-import datetime, hashlib, random
 from mainsite.models import Topic, Message, UserProfile
 
 
@@ -17,35 +18,41 @@ def registration(request):
     if request.method == 'POST':
         # Registration complete, data submitted via POST
         form = UserForm(request.POST)
-        if form.is_valid():  # Cleans form data as well
-            # Accept data and display confirmation
+        if form.is_valid():  # Calling is_valid on ModelForms calls full_clean
+            # Create an inactive User but don't save to database yet
             data = form.cleaned_data
-            new_user = User.objects.create_user(data['username'], data['email'], data['password'])
-            new_user.first_name = data['first_name']
-            new_user.last_name = data['last_name']
-            new_user.is_active = False  # Not active until e-mail activation
-            new_user.save()
+            new_user = User(username=data['username'],
+                            email=data['email'],
+                            password=make_password(data['password']),
+                            first_name=data['first_name'],
+                            last_name=data['last_name'],
+                            is_active=False)
 
-            # Generate activation key and UserProfile
-            salt = hashlib.sha1(str(random.random()).encode('utf-8')).hexdigest()[:5]
-            activation_key = hashlib.sha1((salt+new_user.email).encode('utf-8')).hexdigest()
-            key_expires = datetime.datetime.today() + datetime.timedelta(2)  # 48 hours
+            # Generate activation key, check for duplicates however astronomically unlikely it would be
+            activation_key = None
+            while activation_key is None or UserProfile.objects.filter(activation_key=activation_key).exists():
+                salt = hashlib.sha1(str(random.random()).encode('utf-8')).hexdigest()[:5]
+                activation_key = hashlib.sha1((salt+new_user.email).encode('utf-8')).hexdigest()
+            key_expires = timezone.now() + timezone.timedelta(2)  # 48 hours
 
-            new_profile = UserProfile(user=new_user,
-                                      activation_key=activation_key,
-                                      key_expires=key_expires)
-            new_profile.save()
-
-            #Send activation key
+            # Send activation key
             email_subject = 'Account Activation'
-            email_body = "Dear %s,\nThank you for signing up. To complete your registration, click the link below.\n\n \
-http://127.0.0.1:8000/mainsite/activation/%s\n\nYours,\nTeam8s" % (new_user.username, activation_key)
-
+            email_body = "Dear %s,\n\nThank you for signing up. To complete your registration, go to the link below.\
+\n\nhttp://127.0.0.1:8000/mainsite/activation/%s\n\nYours,\nTeam8s" % (new_user.username, activation_key)
             send_mail(email_subject,
                       email_body,
                       'no-reply@messageboard.com',
                       [new_user.email],
                       fail_silently=False)
+
+            # E-mail sent, safe to save to database
+            new_user.save()
+
+            # Create UserProfile
+            new_profile = UserProfile(user=new_user,
+                                      activation_key=activation_key,
+                                      key_expires=key_expires)
+            new_profile.save()
 
             return render(request, 'registration/registrationcomplete.html', {'data': data})
         else:
@@ -53,11 +60,12 @@ http://127.0.0.1:8000/mainsite/activation/%s\n\nYours,\nTeam8s" % (new_user.user
             return HttpResponse('Invalid registration information.' + str(form.errors))
     else:
         # Registration not completed, initialize form
-        return render(request, 'registration/registration.html', {'form': UserForm(initial={'email': '@mail.utoronto.ca'})})
+        return render(request, 'registration/registration.html',
+                      {'form': UserForm(initial={'email': '@mail.utoronto.ca'})})
 
 
 def email_activation(request, activation_key):
-    # Check for UserProfile that matches activation_key
+    # Check if there is a UserProfile that matches activation_key
     try:
         user_profile = UserProfile.objects.get(activation_key=activation_key)
     except UserProfile.DoesNotExist:
@@ -65,7 +73,7 @@ def email_activation(request, activation_key):
 
     user = user_profile.user
 
-    # Check if already activated
+    # If already activated, do nothing
     if user.is_active:
         return HttpResponse('Account already activated.')
 
@@ -84,12 +92,13 @@ def login(request):
         user = authenticate(username=request.POST['username'],
                             password=request.POST['password'])
         if user is not None:
-            #login the user
-            auth_login(request, user)
-            #redirect
-            return redirect('/mainsite/messageboard/')
+            if user.is_active:
+                auth_login(request, user)
+                return redirect('/mainsite/messageboard/')
+            else:
+                return HttpResponse('Account not activated. Please check your e-mail for your activation link.')
         else:
-            # Display validation errors
+            # Incorrect user or password
             return HttpResponse('User does not exist or password is incorrect.')
     else:
         return render(request, 'registration/login.html', {'form': AuthenticationForm()})
