30ed6e64c9253a3280e396227ac6662c4ab56aae
mckenn19 <austin.mckennan@mail.utoronto.ca>
2014-10-27 18:19:54 -0400

    Join Group Added

---
+++ messageboard/mainsite/migrations/0007_group_group_password.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mainsite', '0006_group_creator'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='group',
+            name='group_password',
+            field=models.CharField(default='austin', max_length=20),
+            preserve_default=False,
+        ),
+    ]
