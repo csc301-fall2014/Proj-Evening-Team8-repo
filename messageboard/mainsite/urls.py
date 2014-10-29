1973b527e0fc2bf056504eba5d9eca4bd0b52fc2
Koarth <dh.walker@mail.utoronto.ca>
2014-10-28 22:32:41 -0400

    Added some comments, refactored some variable names to adhere to PEP8 conventions.
    Former-commit-id: 1483a2760d9c3cb1c67a37376c281f59de1414b2

--- messageboard/mainsite/urls.py
+++ messageboard/mainsite/urls.py
@@ -2,21 +2,29 @@ from django.conf.urls import url
 from mainsite import views
 
 urlpatterns = [
+
+    # Registration and login
     url(r'^registration/$', views.registration, name='registration'),
     url(r'^activation/(?P<activation_key>\w+)/', views.email_activation, name='activation'),
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout_view, name='logout_view'),
+
+    # Topics and main page
     url(r'^messageboard/$', views.messageboard, name='messageboard'),
     url(r'^messageboard/create/$', views.create_topic, name='create_topic'),
     url(r'^messageboard/(?P<topicid>[0-9]*)/$', views.topic, name='topic'),
 
  
-
+    # Subscriptions
     url(r'^messageboard/(?P<topicid>[0-9]*)/subscribe/$', views.subscribe, name="subscribe"),
     url(r'^messageboard/subscriptions/$', views.subscribed_topics, name='subscribed_topics'),
+
+    # Groups
     url(r'^messageboard/creategroup/$', views.create_group, name='create_group'),
     url(r'^messageboard/group/(?P<groupid>[0-9]*)/$', views.group, name='group'),
     url(r'^messageboard/joinedgroups/$', views.joined_groups, name='joined_groups'),
-    url(r'^messageboard/myview/$', views.myview, name='myview'),
     url(r'^messageboard/joingroup/$', views.join_group, name='join_group'),
+
+    # Table view
+    url(r'^messageboard/myview/$', views.myview, name='myview'),
 ]
