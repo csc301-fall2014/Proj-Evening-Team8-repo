345aa5383e24ffac7b55c0af8007e19e7ee7e737
bensonchan <bensonyk.chan@mail.utoronto.ca>
2014-11-09 18:11:09 -0500

    Removed all dependencies on awesome-avatar
    Former-commit-id: 36428a83fcb0e4a3b004403242e0def9c418385b

--- messageboard/mainsite/models.py
+++ messageboard/mainsite/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from awesome_avatar.fields import AvatarField
+#from awesome_avatar.fields import AvatarField
 
 
 class UserProfile(models.Model):
