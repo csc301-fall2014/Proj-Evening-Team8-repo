b432b8ade30c6ff65b09fa277353bd600406c0ad
AllenYX <allen-yx@hotmail.com>
2014-11-12 16:34:36 -0500

    Fixed merging issues

--- messageboard/mainsite/models.py
+++ messageboard/mainsite/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-<<<<<<< HEAD
-=======
 from django.core.validators import RegexValidator
->>>>>>> subadmins50
 #from awesome_avatar.fields import AvatarField
 
 
