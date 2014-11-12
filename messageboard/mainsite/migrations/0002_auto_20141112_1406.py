58816e5f1bb0d05b5fca4f9d10190ff10e88af47
Koarth <dh.walker@mail.utoronto.ca>
2014-11-12 14:10:29 -0500

    Rebase from master, reset migrations

--- messageboard/mainsite/migrations/0002_auto_20141112_1406.py
+++ messageboard/mainsite/migrations/0002_auto_20141112_1406.py
@@ -8,20 +8,26 @@ import datetime
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mainsite', '0009_auto_20141111_2323'),
+        ('mainsite', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
             model_name='userprofile',
             name='last_notified',
-            field=models.DateTimeField(default=datetime.datetime(2014, 11, 11, 18, 12, 27, 907709)),
+            field=models.DateTimeField(default=datetime.datetime(1990, 11, 12, 14, 6, 32, 138361)),
             preserve_default=True,
         ),
         migrations.AddField(
             model_name='userprofile',
             name='notification_delay',
             field=models.FloatField(default=21600.0),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='userprofile',
+            name='notification_queue',
+            field=models.ManyToManyField(to='mainsite.Topic', related_name='users_to_notify'),
             preserve_default=True,
         ),
     ]
