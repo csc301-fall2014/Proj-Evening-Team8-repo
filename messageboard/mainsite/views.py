c6d83df9d5ef1850d2f1b0b438210e9a50b60d84
aleccool213 <alec_brunelle5@hotmail.com>
2014-11-17 19:11:59 -0500

    almost there

--- messageboard/mainsite/views.py
+++ messageboard/mainsite/views.py
@@ -590,36 +590,34 @@ def viewinvites(request, userid):
 
 #view dm's
 @login_required(login_url='/mainsite/login')
-def viewdirectmessages(request, userid):
+def viewdirectmessages(request):
     user = request.user
     direct_messages_user = user.user_profile.direct_messages.all()
-    return render(request, 'topics/direct_message.html', {'user': user, 'directmessages': direct_messages_user})
+    return render(request, 'topics/direct_message_index.html', {'user': user, 'directmessages': direct_messages_user})
 
 #create new dm or view existing
 @login_required(login_url='/mainsite/login')
-def createmessage(request, userid):
+def createmessage(request):
     user = request.user
     all_users = User.objects.all()
     #find all existing direct message recipients
     all_existing_users = User.objects.none()
     for i in user.user_profile.direct_messages.all():
-        tempUser = User.objects.get(username=i.topic_name)
+        tempUser = User.objects.get(id=i.topic_name)
         all_existing_users.add(tempUser)
 
     if request.method == "POST":
-        if "new" in request.POST:
-            #create new dm
-            new_dm = Topic(topic_name=request.POST['recipient'],
-                creator=user,
-                subscriptions=user,
-                is_direct_message=True)
-            new_dm.save
-            #save dm to user profile set of dm's
-            user.user_profile.direct_messages.add(new_dm)
-            user.save
-            #redirect back to dm index
-            return redirect(reverse('mainsite:messageboard/directmessages'))    
+        #create new dm
+        new_dm = Topic(topic_name=request.POST['recipient'],
+            creator=user,
+            is_direct_message=True)
+        new_dm.save
+        #save dm to user profile set of dm's
+        user.user_profile.direct_messages.add(new_dm)
+        user.save
+        #redirect back to dm index
+        return redirect(reverse('mainsite:messageboard/directmessages'))    
     #render the create new dm page 
-    return render(request, 'topics/new_direct_message.html', {'user': user, 'all_users': all_users,
-     'all_existing_users', all_existing_users})
+    return render(request, 'topics/create_direct_message.html', {'user': user, 'all_users': all_users,
+     'all_existing_users': all_existing_users})
 
